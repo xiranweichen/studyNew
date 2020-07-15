d682114c4a72a475fa637db6578b5f1837df27e6
lizhenhong <18732068538@163.com>
2020-07-15 22:04:28 +0800

    jdk1.7-HashMap之扩容

---
+++ src/main/java/com/study/meite/hashmap/JDK7Test.java
@@ -0,0 +1,49 @@
+package com.study.meite.hashmap;
+
+import com.study.meite.hashmap.ext.JDK7ExtHashMap;
+import org.junit.Test;
+
+import java.util.Map;
+
+/**
+ * @author lizhenhong
+ * @Description:
+ * @date 2020/7/11 10:19
+ */
+public class JDK7Test {
+
+    @Test
+    public void test() {
+        JDK7ExtHashMap extHashMap = new JDK7ExtHashMap<String, String>();
+        extHashMap.put("1号", "1号");// 0
+        extHashMap.put("2号", "1号");// 1
+        extHashMap.put("3号", "1号");// 2
+        extHashMap.put("4号", "1号");// 3
+        extHashMap.put("6号", "1号");// 4
+        extHashMap.put("7号", "1号");
+        extHashMap.put("14号", "1号");
+
+        extHashMap.put("22号", "1号");
+        extHashMap.put("26号", "1号");
+        extHashMap.put("27号", "1号");
+        extHashMap.put("28号", "1号");
+        extHashMap.put("66号", "66");
+        extHashMap.put("30号", "1号");
+        System.out.println("扩容前数据....");
+        extHashMap.print();
+        System.out.println("扩容后数据....");
+        extHashMap.put("31号", "1号");
+        extHashMap.put("66号", "123466666");
+        extHashMap.print();
+        // 修改3号之后
+        System.out.println(extHashMap.get("66号"));
+        // System.out.println("扩容之前获取数据:" + extHashMap.get("1号"));
+        // extHashMap.print();
+        // System.out.println();
+        // // extHashMap.put(14 + "号", 14 + "号");
+        // // extHashMap.put(1 + "号", "4444号");
+        // System.out.println("扩容之后获取数据:" + extHashMap.get("1号"));
+        // extHashMap.print();
+    }
+
+}
--- src/main/java/com/study/meite/hashmap/ext/JDK7ExtHashMap.java
+++ src/main/java/com/study/meite/hashmap/ext/JDK7ExtHashMap.java
@@ -18,14 +18,18 @@ public class JDK7ExtHashMap<K,V> implements ExtMap<K, V> {
     float DEFAULT_LOAD_FACTOR = 0.75f;
 
     // 4. table的默认初始大小：1<<4 = 16
-    static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
+    static int DEFAULT_INITIAL_CAPACITY = 1 << 4;
 
     @Override
     public V put(K key, V value) {
         //1. 判断table是否为空，，如果为空，做初始化
         if (table == null)
             table = new Node[DEFAULT_INITIAL_CAPACITY];
-        //2. 判断是否需要扩容
+        //2. 判断是否需要扩容(判断根据：实际的size >= 负载因子 * 初始容量), 扩容为之前的数组的两倍
+        if (size >= DEFAULT_LOAD_FACTOR * DEFAULT_INITIAL_CAPACITY) {
+            //对table进行扩容
+            reSize();
+        }
         //3. 计算hash，指定下表位置
         int index = getIndex(key, DEFAULT_INITIAL_CAPACITY);
         //判断一下index位置是否有节点元素
@@ -60,6 +64,36 @@ public class JDK7ExtHashMap<K,V> implements ExtMap<K, V> {
         int hashCode = k.hashCode();
         int index = hashCode % length;
         return index;
+    }
+
+    /**
+     * 扩容
+     */
+    private void reSize() {
+        //1. 生成新的table，大小是之前table的两倍
+        Node[] newTable = new Node[DEFAULT_INITIAL_CAPACITY << 1];
+        //2. 重新计算index位置
+        for (int i = 0; i < table.length; i++) {
+            Node<K, V> oldNode = table[i];  //原来的node
+            while (oldNode != null) {
+                table[i] = null;  //让垃圾回收机制回收调table[i](之前的node)
+                K oldKey = oldNode.key;
+                //重新计算index
+                int newIndex = getIndex(oldKey, newTable.length);
+                Node<K, V> oldNext = oldNode.next;
+                //如果index下标在新table
+                //如果newIndex计算结果相同，以链表进行存储(原来的node的下一个是最新的node，也就是原来的node排在前面)
+                oldNode.next = newTable[newIndex];
+                //将之前的node赋值给新的newTable[index]
+                newTable[newIndex] = oldNext;
+                //判断是否继续循环遍历
+                oldNode = oldNext;
+            }
+        }
+        //3. 将数据存放在新的table里面
+        table = newTable;
+        //将DEFAULT_INITIAL_CAPACITY修改为新的table的长度
+        DEFAULT_INITIAL_CAPACITY = newTable.length;
     }
 
     @Override
@@ -123,6 +157,28 @@ public class JDK7ExtHashMap<K,V> implements ExtMap<K, V> {
             this.value = value;
             this.next = next;
         }
+    }
+
+    // 测试方法.打印所有的链表元素
+    public void print() {
+
+        for (int i = 0; i < table.length; i++) {
+            Node<K, V> node = table[i];
+            System.out.print("下标位置[" + i + "]");
+            while (node != null) {
+                System.out.print("[ key:" + node.getKey() + ",value:" + node.getValue() + "]");
+                node = node.next;
+                // if (node.next != null) {
+                // node = node.next;
+                // } else {
+                // // 结束循环
+                // node = null;
+                // }
+
+            }
+            System.out.println();
+        }
+
     }
     
     
