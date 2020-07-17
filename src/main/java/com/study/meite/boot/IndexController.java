701add216b31b067d831876fb72596b490a3f469
zhoushuai <zhoushuai>
2020-07-17 16:49:32 +0800

    让springboot程序跑起来

---
+++ src/main/java/com/study/meite/boot/IndexController.java
@@ -0,0 +1,28 @@
+package com.study.meite.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @date: 2020/7/16 18:57
+ * @author: lizhenhong
+ */
+@RestController //目的：这个注解相当于@ResponseBody+  @Controller, 使这个类所有的方法都返回json格式
+@EnableAutoConfiguration //作用：开启自动装配到spring的bean容器中，加载pom中的jar包信息
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "真的是没有想到，这么简单的一个启动程序竟然搞了这么久，\n" +
+                "这是我目前为止发现的idea2020.1的唯一坑点";
+    }
+
+    public static void main(String[] args) {
+        //告诉 springboot 程序入口
+        SpringApplication.run(IndexController.class, args);
+    }
+    
+}
