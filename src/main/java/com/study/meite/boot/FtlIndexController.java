0bc7d4c11f49a0dc62ed727dd0e31d28bf5fae51
lizhenhong <18732068538@163.com>
2020-07-18 14:28:06 +0800

    模拟开发测试生产环境配置文件数据读取

--- src/main/java/com/study/meite/boot/FtlIndexController.java
+++ src/main/java/com/study/meite/boot/FtlIndexController.java
@@ -1,18 +1,32 @@
 package com.study.meite.boot;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author lizhenhong
  * @Description: 整合ftl页面
  * @date 2020/7/18 11:56
  */
-@Controller
+@RestController
+@RequestMapping("/ftlIndex")
 public class FtlIndexController {
 
+    @Value("${http_url}")
+    private String http_url;
+
     @RequestMapping("ftlIndex")
-    public String ftlIndex() {
+    public String getFtlIndex() {
         return "index";
     }
+
+    @RequestMapping("/getHttp_url")
+    @ResponseBody
+    public String getHttp_url() {
+        return http_url;
+    }
+
+
 }
