3cf5826aad1a82712fe731fe3a330e064f3b9381
zhoushuai <zhoushuai>
2020-07-20 19:58:44 +0800

    暂时把报错的地方注释了，以后再说

--- src/main/java/com/study/meite/boot/manyDataSource/config/DBConfig2.java
+++ src/main/java/com/study/meite/boot/manyDataSource/config/DBConfig2.java
@@ -1,29 +1,29 @@
-package com.study.meite.boot.manyDataSource.config;
-
-import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
-
-/**
- * @author lizhenhong
- * @Description: 读取配置文件信息(中读取mysql.datasource.users数据源)
- * @date 2020/7/19 21:30
- */
-@Data
-@ConfigurationProperties(prefix = "mysql.datasource.users")  //从配置文件中读取mysql.datasource.users数据源
-@Component
-public class DBConfig2 {
-
-    private String url;
-    private String username;
-    private String password;
-    private int minPoolSize;
-    private int maxPoolSize;
-    private int maxLifetime;
-    private int borrowConnectionTimeout;
-    private int loginTimeout;
-    private int maintenanceInterval;
-    private int maxIdleTime;
-    private String testQuery;
-
-}
+//package com.study.meite.boot.manyDataSource.config;
+//
+//import lombok.Data;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * @author lizhenhong
+// * @Description: 读取配置文件信息(中读取mysql.datasource.users数据源)
+// * @date 2020/7/19 21:30
+// */
+//@Data
+//@ConfigurationProperties(prefix = "mysql.datasource.users")  //从配置文件中读取mysql.datasource.users数据源
+//@Component
+//public class DBConfig2 {
+//
+//    private String url;
+//    private String username;
+//    private String password;
+//    private int minPoolSize;
+//    private int maxPoolSize;
+//    private int maxLifetime;
+//    private int borrowConnectionTimeout;
+//    private int loginTimeout;
+//    private int maintenanceInterval;
+//    private int maxIdleTime;
+//    private String testQuery;
+//
+//}
